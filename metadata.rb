bbd91687941a16bedb9dc39e2c7c99fa55f21b98
valenbb <valenbb@gmail.com>
2017-02-26 18:48:10 -0500

    Removed merge header

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,9 @@ maintainer_email 'valenbb@gmail.com'
 license 'all_rights'
 description 'Base bootstrap cookbook'
 long_description 'Base bootstrap cookbook for ESN'
-<<<<<<< HEAD
-version '0.1.2'
-=======
 version '0.1.3'
 
 depends 'esnspacewalk', '0.1.0'
->>>>>>> release/v0.1.3
 
 issues_url 'https://github.com/valenbb/esnbootstrap/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/valenbb/esnbootstrap' if respond_to?(:source_url)
